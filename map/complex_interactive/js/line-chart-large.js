9a1eb8388d5e5ce6bd04be21af53ca3451d68aa5
Barbara Kulaga <barbara.kulaga76@gmail.com>
2022-07-20 00:25:38 +1000

    changes to tooltip colour in line charts and in the map interactive

--- map/complex_interactive/js/line-chart-large.js
+++ map/complex_interactive/js/line-chart-large.js
@@ -71,6 +71,7 @@ class LineChartLarge {
 
         svg.call(tip);
 
+
         svg.append("path")
             .data([vis.data])
             .attr("class", "line")
@@ -92,85 +93,6 @@ class LineChartLarge {
             .call(d3.axisLeft(y));
 
 
-        let tooltip = svg.append("g")
-            .attr("display", "none")
-            .attr("class", "tooltip-group");
-
-        tooltip.append("line")
-            .attr("stroke", "black")
-            .attr("stroke-width", 1)
-            .attr("x1", 0)
-            .attr("y1", height)
-            .attr("x2", 0)
-            .attr("y2", 0);
-
-
-        let text = tooltip.append("text")
-            .attr("class", "tooltip-text")
-            .attr("x", 5)
-            .attr("y", 40)
-            .style("font-size", '10')
-            .style("fill", "black");
-
-        let text2 = tooltip.append("text")
-            .attr("class", "tooltip-text")
-            .style("font-size", '10')
-            .attr("x", 5)
-            .attr("y", 60)
-            .style("fill", "black");
-
-        let text3 = tooltip.append("text")
-            .attr("class", "tooltip-text")
-            .style("font-size", '10')
-            .attr("x", 5)
-            .attr("y", 80)
-            .style("fill", "black");
-
-        let text4 = tooltip.append("text")
-            .attr("class", "tooltip-text")
-            .style("font-size", '10')
-            .attr("x", 5)
-            .attr("y", 100)
-            .style("fill", "red");
-
-        let overlay = svg.append("rect")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("fill", "transparent")
-            .on("mouseover", function (event, d) {
-                tooltip.attr("display", "null");
-            })
-            .on("mouseout", function (event, d) {
-                tooltip.attr("display", "none");
-            })
-            .on("mousemove", mousemove);
-
-        let bisectDate = d3.bisector(d=>d.date).left;
-        let formatTime = d3.timeFormat("%Y-%m-%d");
-
-        const yearFormat = d3.timeFormat("%Y");
-
-        const x_time = d3.scaleTime()
-            .domain(d3.extent(vis.data, function(d) { return d.date; }))
-            .range([0, width]);
-
-        function mousemove(event) {
-            let x_coordinate = d3.pointer(event)[0];
-            let x_date = x_time.invert(x_coordinate);
-            let index = bisectDate(vis.data, x_date);
-            let closest = vis.data[index];
-
-
-            tooltip.attr("transform", "translate(" + x_coordinate + ")")
-            text.text("State: " + (closest.State2));
-            text2.text("Date: " + formatTime(closest.date));
-            text3.text("New Cases: " + closest.value);
-            text4.text("7-Day New Cases Avg: " + (closest.value2.toFixed(0)));
-
-
-        }
 
 
         var keys = [" "]
@@ -218,6 +140,99 @@ class LineChartLarge {
             .text("7-Day New Cases Average")
             .attr("font-family", "Segoe UI")
             .attr("font-size", "9");
+
+
+
+        let tooltip = svg.append("g")
+            .attr("display", "none")
+            .attr("class", "tooltip-group");
+
+
+        tooltip.append("rect")
+            .attr("width", 140)
+            .attr("height", height-200)
+            .attr("x", 0)
+            .attr("y", 30)
+            .style("fill", "white")
+            .attr("class","tool-rect-background")
+
+
+        tooltip.append("line")
+            .attr("stroke", "black")
+            .attr("stroke-width", 1)
+            .attr("x1", 0)
+            .attr("y1", height)
+            .attr("x2", 0)
+            .attr("y2", 0);
+
+
+        let text = tooltip.append("text")
+            .attr("class", "tooltip-text")
+            .attr("x", 5)
+            .attr("y", 60)
+            .style("font-size", '10')
+            .style("fill", "black");
+
+        let text2 = tooltip.append("text")
+            .attr("class", "tooltip-text")
+            .style("font-size", '10')
+            .attr("x", 5)
+            .attr("y", 80)
+            .style("fill", "black");
+
+        let text3 = tooltip.append("text")
+            .attr("class", "tooltip-text")
+            .style("font-size", '10')
+            .attr("x", 5)
+            .attr("y", 100)
+            .style("fill", "black");
+
+        let text4 = tooltip.append("text")
+            .attr("class", "tooltip-text")
+            .style("font-size", '10')
+            .attr("x", 5)
+            .attr("y", 120)
+            .style("fill", "red");
+
+        let overlay = svg.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("fill", "transparent")
+            .on("mouseover", function (event, d) {
+                tooltip.attr("display", "null");
+            })
+            .on("mouseout", function (event, d) {
+                tooltip.attr("display", "none");
+            })
+            .on("mousemove", mousemove);
+
+        let bisectDate = d3.bisector(d=>d.date).left;
+        let formatTime = d3.timeFormat("%Y-%m-%d");
+
+        const yearFormat = d3.timeFormat("%Y");
+
+        const x_time = d3.scaleTime()
+            .domain(d3.extent(vis.data, function(d) { return d.date; }))
+            .range([0, width]);
+
+        function mousemove(event) {
+            let x_coordinate = d3.pointer(event)[0];
+            let x_date = x_time.invert(x_coordinate);
+            let index = bisectDate(vis.data, x_date);
+            let closest = vis.data[index];
+
+
+            tooltip.attr("transform", "translate(" + x_coordinate + ")")
+            text.text("State: " + (closest.State2));
+            text2.text("Date: " + formatTime(closest.date));
+            text3.text("New Cases: " + closest.value);
+            text4.text("7-Day New Cases Avg: " + (closest.value2.toFixed(0)));
+
+
+        }
+
 
 
     }
